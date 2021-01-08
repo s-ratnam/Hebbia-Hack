2c5d86e98e8a5976cebecf855c975d35df591b4e
Sheryl R <sratnam@ucsc.edu>
2021-01-07 17:39:04 -0800

    adding project files

---
+++ hebbia-project/server/routes/search.ts
@@ -0,0 +1,25 @@
+import * as express from 'express';
+const { Router } = require('express');
+const path = require('path');
+const { get } = require('axios');
+
+const router: express.Router = Router();
+
+router.get('*', (req: express.Request, res: express.Response, next: express.NextFunction): void => {
+  res.sendFile(path.resolve('../', 'build/index.html'));
+});
+
+router.post('/:query/:start?', async (req: express.Request, res: express.Response) => {
+  const { query, start } = req.params;
+  try {
+    // const { data: { items } } = await get(`
+    // https://www.googleapis.com/customsearch/v1?key=AIzaSyBQ8fEfrYFxKZeJLEH5R-P4MMRAhnwOdLg&cx=000950167190857528722:vf0rypkbf0w&start=${start
+    //   ? start
+    //   : 10}&q=${encodeURI(query)}`);
+    res.send("1");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+export const SearchController = router;
